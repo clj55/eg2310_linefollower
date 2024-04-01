9a3d18229b0d254cf0e0738286813038b5a1123b
WAAAJIAN <157587457+WAAAJIAN@users.noreply.github.com>
2024-04-02 01:04:32 +0800

    Update line_follower.py
    redo every part of the line following plan.
    
    uses a rate to control the motors to follow a line towards 
    a bucket.
    
    added functionalities to the robot as well

--- line_follower.py
+++ line_follower.py
@@ -3,86 +3,71 @@ from rclpy.node import Node
 import geometry_msgs.msg
 import RPi.GPIO as GPIO
 from time import sleep, time
+import requests
+import math
+from threading import Timer
 
-# Set pin numbering convention
+# setting up all the GPIO for functionality
 GPIO.setmode(GPIO.BCM)
-# Choose an appropriate pwm channel to be used to control the servo
 ir1_pin = 17
 ir2_pin = 27
-servo1 = 25
-servo2 = 21
-motor = 12
-# Set the pin as an output
+servo1_pin = 25
+servo2_pin = 21
+
 GPIO.setup(ir1_pin, GPIO.IN)
 GPIO.setup(ir2_pin, GPIO.IN)
-GPIO.setup(servo1, GPIO.OUT)
-GPIO.setup(servo2, GPIO.OUT)
-GPIO.setup(motor, GPIO.OUT)
-
-
-s1 = GPIO.PWM(servo1, 50)
-s2 = GPIO.PWM(servo2, 50)
-m = GPIO.PWM(motor, 50)
-open_angle = 90
-close_angle = 0
-
-# constants
-rotatechange = 0.3
-speedchange = -0.03
-
+GPIO.setup(servo1_pin, GPIO.OUT)
+GPIO.setup(servo2_pin, GPIO.OUT)
 
 def convert_ang_dc(angle):
     dc = 2.5 + angle / 18 
     return dc
 
+# Function: Request HTTP
+esp32_ip = '172.20.10.5'
+turtleBot_ID = 43
+header = {'Content-Type': 'application/json'}
 
-class Mover(Node):
-    def __init__(self):
+# Class 1: Functionality of the turtleBot3 Burger
+class Func(Node):
+    # initialiser
+    def __init__():
         super().__init__('mover')
-        self.publisher_ = self.create_publisher(geometry_msgs.msg.Twist,'cmd_vel',10)
-    
-    def read_ir(self):
-        twist = geometry_msgs.msg.Twist()
-        try:
-            counter = 0
-            motor_start = False
-            s1open = False
-            s2open = False
-            open_dc= convert_ang_dc(open_angle)
-            close_dc = convert_ang_dc(close_angle)
+        self.publisher_ = self.create_publisher(geometry_msgs.msg.Twist, 'cmd_vel', 10)
 
+    # Function 1: Line_follower
+    def line_follower(self):
+        twist = geometry_msgs.msg.Twist()
+        rad_per_sec = 0.3
+        cent_per_sec = 0.03
+        rate = 0.1          
+        try:
             while True:
                 ir1 = GPIO.input(ir1_pin)
                 ir2 = GPIO.input(ir2_pin)
                 print(f'ir1: {ir1} | ir2: {ir2}')
-
-                twist.linear.x = speedchange
-
-
-                #TODO: Control Servos and Motors
+                twist.linear.x = cent_per_sec
+                self.publisher_.publish(twist)
+                
                 if ir1 == 1 and ir2 == 1:
                     twist.linear.x = 0.0
-                    twist.angular.z = 0.0
-                    counter += 1
-                    last_time = time.time()
+                    twist.linear.z = 0.0
+  
+                elif ir1 == 1 and ir2 == 0:
+                    twist.linear.x -= cent_per_sec * rate
+                    twist.linear.z += rad_per_sec
+                    if twist.linear.x <= 0:
+                        twist.linear.x = 0
 
-                    if motor_start == False:
-                         m.start(50)
-                    elif s1open == False:
-                        s1.ChangeDutyCycle(open_dc)
-                        s1open = True
-                    elif (last_time - time.time()) > 1:
-                        s2.ChangeDutyCycle(open_dc)
-                        s2open = True
-                    
-                elif ir1 == 1:
-                    twist.angular.z += rotatechange
-                    sleep(0.5)
-                elif ir2 == 1:
-                    twist.angular.z -= rotatechange
-                    sleep(0.5)
+                elif ir2 == 1 and ir1 == 0:
+                    twist.linear.x -= cent_per_sec * rate
+                    twist.linear.z -= rad_per_sec
+                    if twist.linear.x <= 0:
+                        twist.linear.x = 0
+
                 else: 
-                    twist.angular.z = 0.0
+                    twist.linear.y = 0.0
+                    twist.linear.x = cent_per_sec
                 
                 self.publisher_.publish(twist)
         
@@ -95,22 +80,76 @@ class Mover(Node):
             twist.linear.x = 0.0
             twist.angular.z = 0.0
             self.publisher_.publish(twist)
+    
+    # Function 2: Ball Dispenser
+    def ball_dispenser():
+        s1 = GPIO.PWM(servo1_pin, 50)
+        s2 = GPIO.PWM(servo2_pin, 50)
+        counter = 0
 
+        while counter <= 10:
+            s1.ChangeDutyCycle(convert_ang_dc(90))
+            s2.ChangeDutyCycle(convert_ang_dc(90))
+            counter += 1
+        
+        s1.ChangeDutyCycle(convert_ang_dc(0))
+        s2.ChangeDutyCycle(convert_ang_dc(0))
+
+    # Function 3: HTTP request to ESP32 --> needs to be tested as I didn't test it as a function
+    def send_request(self, ip_address, ID):
+        header = {'Content-Type': 'application/json'}
+        endpoint = "http://" + ip_address + "/openDoor"
+        data = "action": "openDoor", "parameters": {"robotId": ID}}
+        response = requests.post(url=endpoint, json=data, headers=header)
+        return(response.text)
+
+    # Function 4: Specific turns in radians (turning within its own space)
+    def turn_specific(self, radian):
+        twist = geometry_msgs.msg.Twist()
+        if radian < 0:
+            rad_per_sec = -0.3
+        else:
+            rad_per_sec = 0.3
+        time_90 = abs(radian / rad_per_sec)
+        twist.angular.z = rad_per_sec
+        th.Timer(time_90, twist.angular.z = 0.0)
+
+    # Function 5: Marker Detector --> this one needs to be implemented with the Frontier and A* algo?
+    def marker_detector(self):
+        # twist = geometry_msgs.msg.Twist()
+        # ir1 = GPIO.input(ir1_pin)
+        # ir2 = GPIO.input(ir2_pin)
+        # if ir1 == 1 and ir2 == 1:
+        #     twist.linear.x = 0.0
+        #     self.publisher_.publish(twist)
+        pass
+        
 
 def main(args=None):
     rclpy.init(args=args)
-
-    mover = Mover()
-    mover.read_ir()
-
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    mover.destroy_node()
+    TurtleBot = Func()
+    # finds the marker
+    TurtleBot.marker_detector()
+    # sends HTTP request to determine which door to open
+    response = 'no'
+    while response == 'no':
+        response = TurtleBot.send_request(esp32_ip, turtleBot_ID)
+        if response == 'door1':
+            TurtleBot.turn_specific(math.pi / 2)
+            break
+        elif response == 'door2':
+            TurtleBot.turn_specific(- math.pi / 2)
+            break
+        else:
+            th.Timer(60, response = 'no')
+    # line follows to the bucket
+    TurtleBot.line_follower()
+    # dispense balls
+    TurtleBot.ball_dispenser()
+    # turns 180 degrees
+    TurtleBot.turn_specific(math.pi)
+    # follows line backward
+    TurtleBot.line_follower()
     
-    rclpy.shutdown()
-
-
 if __name__ == '__main__':
     main()
